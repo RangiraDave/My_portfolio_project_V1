b3303e85cebad82ac93973733d288bca42827efb
RangiraDave <rangiradev666@gmail.com>
2024-04-30 07:57:36 +0200

    started working on the universities dynamic page

--- app/__init__.py
+++ app/__init__.py
@@ -10,11 +10,12 @@ from .models import db
 
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'uruni_urufunguzo_ruhishe'
 tp = 'mysql+pymysql://portfolio:Password%40123@localhost/application_copilot'
 app.config['SQLALCHEMY_DATABASE_URI'] = tp
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+CHATGPT_API_KEY = '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'
 
 db.init_app(app)
 migrate = Migrate(app, db)
