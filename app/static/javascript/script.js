c17cfed54bbc12ae7a6a8f240ca04890c6a99ecb
RangiraDave <rangiradev666@gmail.com>
2024-05-07 21:01:16 +0200

    I added the like and dislike buttons in the universities page

--- app/static/javascript/script.js
+++ app/static/javascript/script.js
@@ -1,4 +1,64 @@
 $(document).ready(function() {
+    // Like button click event
+    $('.like-btn').click(function() {
+        var universityId = $(this).data('university-id');
+        // Send Ajax request to like endpoint
+        $.ajax({
+            type: 'POST',
+            url: '/like',
+            data: {
+                university_id: universityId
+            },
+            success: function(response) {
+                // Handle success (e.g., update UI)
+            },
+            error: function(xhr, status, error) {
+                // Handle error
+            }
+        });
+    });
+
+    // Dislike button click event
+    $('.dislike-btn').click(function() {
+        var universityId = $(this).data('university-id');
+        // Send Ajax request to dislike endpoint
+        $.ajax({
+            type: 'POST',
+            url: '/dislike',
+            data: {
+                university_id: universityId
+            },
+            success: function(response) {
+                // Handle success (e.g., update UI)
+            },
+            error: function(xhr, status, error) {
+                // Handle error
+            }
+        });
+    });
+
+    // Adding the search button click handler
+    $('#searchButton').click(function() {
+        console.log('Search button clicked!')
+        var question = $('#searchField').val();
+        console.log('Question:', question);
+        $.ajax({
+            url: '/search',
+            data: { 'question': question },
+            type: 'GET',
+            success: function(response) {
+                if (response.success) {
+                    $('#searchResults').html(response.answer);
+                } else {
+                    $('#searchResults').html(`An error occurred while searching for the answer.${response.error}`);
+                }
+            }
+        });
+    });
+});
+
+
+$(document).ready(function() {
     $('form').submit(function(event) {
         event.preventDefault();  // Prevent the form from submitting
         $.ajax({
