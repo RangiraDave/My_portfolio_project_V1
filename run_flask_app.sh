e456b61190efbdb7c517065a508d0ff6607c1445
RangiraDave <rangiradev666@gmail.com>
2024-05-10 20:33:14 +0200

    Added script to queitly install requirements

--- run_flask_app.sh
+++ run_flask_app.sh
@@ -10,5 +10,8 @@ source venv/bin/activate
 # Running the flask app
 # flask run --host=0.0.0.0 --port=5000
 
+# Quietly install requirements
+pip install -r -q requirements.txt
+
 # Running flask app using gunicorn
 venv/bin/gunicorn -b 0.0.0.0:5000 -w 4 app:app
